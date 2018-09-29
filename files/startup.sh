9fae9ab91a2498a1c06a54791890e2d4735a9ff2
Jarett Stevens <jarett.stevens@gmail.com>
2018-09-28 20:40:32 -0500

    set default sound card

--- files/startup.sh
+++ files/startup.sh
@@ -5,5 +5,16 @@ if ip route | grep -q default ; then
 else
   xterm -bg black -fg grey -bc -e /home/quake3/network-msg.sh
 fi
+
+# default soundcard helper
+sound_card="$(aplay -l | sed -n -e 's/^card \([0-9]\):.* Analog .*$/\1/p' | head -1)"
+
+if test -n "${sound_card}" ; then
+  sudo tee /etc/asound.conf >/dev/null <<- EOF
+	defaults.pcm.card ${sound_card}
+	defaults.ctl.card ${sound_card}
+	EOF
+fi
+
 quake3
 exit 0
